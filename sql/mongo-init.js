3f79aa97aba79623ed4bac825622cd85300c2c17
polmustdie <nesterovapolina77@gmail.com>
2023-06-15 20:22:39 +0300

    containers run

---
+++ sql/mongo-init.js
@@ -0,0 +1,13 @@
+// db = db.getSiblingDB("testDB");
+//
+// db.createCollection('documentsCollection');
+//
+// db.documentsCollection.insertMany([
+//     { "id": 1,
+//     "fieldName": "testFieldName",
+//     "fieldvValue": "testFieldValue",
+//     "fieldBoolean": true,
+//     "fieldArrays": Arrays.asList("test1", "test2", "test3")},
+//     { "testFieldName": "testName",
+//     "condition_field_in_mongo": "condition_value"}
+// ]);
